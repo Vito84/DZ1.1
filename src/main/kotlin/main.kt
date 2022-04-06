f8dabb9a3bdefd3c486ba20dc4b056f7c5c9bc20
Viktor Anikin <Avka3d@mail.ru>
2022-04-06 16:10:46 +0500

    code editing

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,28 @@
 package ru.netology
 
-fun main() {
-    val amount = 100000
-    val minCommission = 3500
-    val commission = ((amount / 100) * 0.75).toInt()
-    println(
-        if (commission < minCommission) "Ваша комиссия составляет 3500 копеек"
-        else "Ваша комиссия состовляет $commission копеек"
-    )
+const val COMMISSION = 0.75
+const val MIN_COMMISSION = 35
 
+fun main() {
+    amount(COMMISSION, MIN_COMMISSION)
 
 }
+
+fun amount(
+    COMMISSION: Double,
+    MIN_COMMISSION: Int
+) {
+    print("Введите сумму перевода \n")
+    val transferAmount = readln()?.toInt()
+    val result = ((transferAmount * COMMISSION) / 100)
+    val rub = result.toInt()
+    val kop = (result * 100).toInt() % 100
+
+    println(
+        if (result < MIN_COMMISSION) "Ваша комиссия составляет 35 рублей 00 копеек"
+        else "Ваша комиссия состовляет $rub рублей $kop копеек"
+    )
+}
+
+
+
